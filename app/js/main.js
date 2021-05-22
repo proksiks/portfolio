7263cab85ac1a20204b990cb3313f569b213a5f5
Volodya <proksik49032@mail.ru>
2021-05-22 16:07:51 +0400

    remove-main-tabs

--- app/js/main.js
+++ app/js/main.js
@@ -269,7 +269,26 @@ $(document).ready(function () {
 });
 //// SWIPER
 
-
+///COUNT
+var show = true;
+var countbox = ".info-holder__stat";
+$(window).on("scroll load resize", function () {
+  if (!show) return false;
+  var w_top = $(window).scrollTop();
+  var e_top = $(countbox).offset().top;
+  var w_height = $(window).height();
+  var d_height = $(document).height();
+  var e_height = $(countbox).outerHeight();
+  if (w_top + 900 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+    $('.info-holder__num').css('opacity', '1');
+    $('.info-holder__num').spincrement({
+      thousandSeparator: "",
+      duration: 7000
+    });
+    show = false;
+  }
+});
+///COUNT
 
 //ABOUT__PAGE
 
@@ -287,4 +306,5 @@ let ReviewParralaxScene = new Parallax(ReviewStars);
 ////Parallax
 
 //ЗАКИНУТЬ НА ОСНОВНУЮ СТРАНИЦУ
+
 //# sourceMappingURL=main.js.map
