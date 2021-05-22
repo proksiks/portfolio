4f8ad608ac301ba230d16d6353c972fa47f89853
Volodya <proksik49032@mail.ru>
2021-05-22 16:04:24 +0400

    tabs

--- app/js/main.js
+++ app/js/main.js
@@ -269,26 +269,7 @@ $(document).ready(function () {
 });
 //// SWIPER
 
-////TABS
-const tabs = document.querySelector(".wrapper");
-const tabButton = document.querySelectorAll(".general-tabs__numbers");
-const contents = document.querySelectorAll(".general-tabs__bot-steps");
-tabs.onclick = e => {
-  const id = e.target.dataset.id;
-  if (id) {
-    tabButton.forEach(btn => {
-      btn.classList.remove("_active");
-    });
-    e.target.classList.add("_active");
 
-    contents.forEach(content => {
-      content.classList.remove("_active");
-    });
-    const element = document.getElementById(id);
-    element.classList.add("_active");
-  }
-}
-////TABS
 
 //ABOUT__PAGE
 
